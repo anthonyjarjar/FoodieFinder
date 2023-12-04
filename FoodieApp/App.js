8c55666c0d3593e034560dcf9d86ed02d7508059
anthonyjarjar <anthjar203@gmail.com>
2023-12-04 12:33:16 -0800

    Added loading screen to app

--- FoodieApp/App.js
+++ FoodieApp/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, Image, TouchableOpacity, StyleSheet, Platform } from 'react-native';
+import { View, Text, Image, TouchableOpacity, StyleSheet, Platform, ActivityIndicator } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import * as ImagePicker from 'expo-image-picker';
 import { ScrollView, GestureHandlerRootView } from 'react-native-gesture-handler';
@@ -7,6 +7,7 @@ import { ScrollView, GestureHandlerRootView } from 'react-native-gesture-handler
 export default function App() {
   const [image, setImage] = useState(null);
   const [prediction, setPrediction] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     (async () => {
@@ -123,71 +124,76 @@ export default function App() {
     100: 'Waffles'
 }
 
-  const pickImage = async () => {
-    console.log('Uploading image...');
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
-      allowsEditing: true,
-      aspect: [4, 3],
-      quality: 1,
-    });
-  
-    if (!result.cancelled) {
-      console.log('Image URI:', result.uri);
-      setImage(result.uri);
-    }
+const pickImage = async () => {
+  console.log('Uploading image...');
+  setLoading(true);
 
-    predictImage(result.uri)
-    
-  };
-  
-  
-  const predictImage = async (selectedImageURI) => {
-    console.log('Predicting image...', selectedImageURI);
-    const formData = new FormData();
-    formData.append('image', { uri: selectedImageURI, name: 'image.jpg', type: 'image/jpeg' });
-  
-    try {
-      const response = await fetch('http://192.168.1.69:8000/predict', {
-        method: 'POST',
-        body: formData,
-        headers: {
-          'Content-Type': 'multipart/form-data',
-        },
-      });
-  
-      const result = await response.json();
-      console.log('Server Response:', result);
-  
-      const predictionLabel = result;
-      const foodPrediction = foodLabels[predictionLabel];
-      
-      console.log(predictionLabel, foodPrediction);
-      setPrediction(foodPrediction);
-    } catch (error) {
-      console.error('Error predicting image:', error);
-    }
-  };
-  
-  
-  
-  return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <ScrollView contentContainerStyle={styles.container}>
-        <Text style={styles.title}>Foodie Finder</Text>
-        <TouchableOpacity style={styles.uploadButton} onPress={pickImage}>
-          <Text style={styles.buttonText}>Upload Image</Text>
-        </TouchableOpacity>
-        {image && (
+  let result = await ImagePicker.launchImageLibraryAsync({
+    mediaTypes: ImagePicker.MediaTypeOptions.Images,
+    allowsEditing: true,
+    aspect: [4, 3],
+    quality: 1,
+  });
+
+  if (!result.cancelled) {
+    console.log('Image URI:', result.uri);
+    setImage(result.uri);
+    predictImage(result.uri);
+  } else {
+    setLoading(false);
+  }
+};
+
+const predictImage = async (selectedImageURI) => {
+  console.log('Predicting image...', selectedImageURI);
+  const formData = new FormData();
+  formData.append('image', { uri: selectedImageURI, name: 'image.jpg', type: 'image/jpeg' });
+
+  try {
+    const response = await fetch('http://192.168.1.101:8000/predict', {
+      method: 'POST',
+      body: formData,
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    });
+
+    const result = await response.json();
+    console.log('Server Response:', result);
+
+    const predictionLabel = result;
+    const foodPrediction = foodLabels[predictionLabel];
+
+    console.log(predictionLabel, foodPrediction);
+    setPrediction(foodPrediction);
+  } catch (error) {
+    console.error('Error predicting image:', error);
+  } finally {
+    setLoading(false);
+  }
+};
+
+return (
+  <GestureHandlerRootView style={{ flex: 1 }}>
+    <ScrollView contentContainerStyle={styles.container}>
+      <Text style={styles.title}>Foodie Finder</Text>
+      <TouchableOpacity style={styles.uploadButton} onPress={pickImage}>
+        <Text style={styles.buttonText}>Upload Image</Text>
+      </TouchableOpacity>
+      {loading ? (
+        <ActivityIndicator size="large" color="#3498db" />
+      ) : (
+        image && (
           <View style={styles.imageContainer}>
             <Image source={{ uri: image }} style={styles.image} resizeMode="contain" />
             {prediction && <Text style={styles.prediction}>{prediction}</Text>}
           </View>
-        )}
-        <StatusBar style="auto" />
-      </ScrollView>
-    </GestureHandlerRootView>
-  );
+        )
+      )}
+      <StatusBar style="auto" />
+    </ScrollView>
+  </GestureHandlerRootView>
+);
 }
 
 const styles = StyleSheet.create({
